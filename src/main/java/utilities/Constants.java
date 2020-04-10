ea6569ba1d214a94552512d3bdd90646d98b03aa
love <lovesoni607@gmail.com>
2020-04-10 12:46:54 +0530

    Added directory constants

---
+++ src/main/java/utilities/Constants.java
@@ -0,0 +1,19 @@
+package utilities;
+
+import java.io.File;
+
+public class Constants {
+        public static String PROJECT_PATH = System.getProperty("user.dir");
+
+        public static String slash = File.separator;
+
+        public static String SRC_DIRECTORY_PATH = PROJECT_PATH + slash + "src";
+
+        public static String MAIN_DIRECTORY_PATH = SRC_DIRECTORY_PATH + slash + "main";
+
+        public static String JAVA_DIRECTORY_PATH = MAIN_DIRECTORY_PATH + slash + "java";
+
+        public static String RESOURCE_DIRECTORY_PATH = JAVA_DIRECTORY_PATH + slash + "resources";
+
+        public static String CAPABILITIES_DIRECTORY_PATH = RESOURCE_DIRECTORY_PATH + slash + "capabilities";
+}
