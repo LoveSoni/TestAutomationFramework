79b59ba2ce83cf46ae0a64ba1eb394f718755449
love <lovesoni607@gmail.com>
2020-04-10 11:16:45 +0530

    Added java utility

---
+++ src/main/java/utilities/JavaUtility.java
@@ -0,0 +1,15 @@
+package utilities;
+
+import org.apache.log4j.Logger;
+
+public class JavaUtility {
+    private static Logger logger = Logger.getLogger(JavaUtility.class);
+
+    public static void sleep(int seconds){
+        try{
+            Thread.sleep(1000*seconds);
+        }catch (InterruptedException e){
+            logger.error(e.getMessage());
+        }
+    }
+}
