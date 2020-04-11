8973a3b31b257c7b9ca4d9c4ed9fced7245f93fb
love <lovesoni607@gmail.com>
2020-04-11 17:59:37 +0530

    Web session manager

--- src/main/java/sessions/WebSession.java
+++ src/main/java/sessions/WebSession.java
@@ -1,9 +1,26 @@
 package sessions;
 
+import io.appium.java_client.AppiumDriver;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
 /**
  * Love
  */
-public class WebSession {
+public class WebSession{
+    private WebDriver driver;
+    public void initiateDriver(){
+
+    }
+
+    public AppiumDriver getDriver(){
+        return (AppiumDriver) this.driver;
+    }
+
+    public DesiredCapabilities clientCapabilities(){
+
+    }
+
     public void startSession(){
 
     }
