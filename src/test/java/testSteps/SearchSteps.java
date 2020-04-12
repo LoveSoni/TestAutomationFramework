db56bbb4ee243f174fee30551e2e31aa8a7edd08
love <lovesoni607@gmail.com>
2020-04-12 15:02:45 +0530

    enter prefil query

--- src/test/java/testSteps/SearchSteps.java
+++ src/test/java/testSteps/SearchSteps.java
@@ -7,25 +7,30 @@ package testSteps;
 import library.AppiumLibrary;
 import sessions.SessionManager;
 import utilities.PropertyReader;
-
 import java.util.Properties;
 
 public class SearchSteps {
     private AppiumLibrary appiumLibrary;
     private Properties searchScreen = PropertyReader.readLocatorProperties("");
 
-    public SearchSteps(SessionManager sessionManager)
-    {
+    public SearchSteps(SessionManager sessionManager) {
         appiumLibrary = new AppiumLibrary(sessionManager);
     }
 
-    public void selectInvokeSearch(){
+    public void selectInvokeSearch() {
         appiumLibrary.click(searchScreen.getProperty("invokeSearch"));
     }
 
-    public void selectQuerySearhResults(){
+    public void selectQuerySearhResults() {
         appiumLibrary.click(searchScreen.getProperty("querySearchResult"));
     }
 
+    public void clickOnSearchRequested() {
+        appiumLibrary.click(searchScreen.getProperty("onSearchRequested"));
+    }
+
+    public void enterPrefilQuery(String text) {
+        appiumLibrary.enterText(searchScreen.getProperty("txt_query_prefill"), text);
+    }
 
 }
