153f62094bc9b3ed0a4e34ed9d9363b39112afce
love <lovesoni607@gmail.com>
2020-04-16 23:03:30 +0530

    get jsonfile as json object

---
+++ src/main/java/utilities/JsonUtility.java
@@ -0,0 +1,35 @@
+package utilities;
+
+import org.apache.log4j.Logger;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class JsonUtility {
+
+    private static Logger logger = Logger.getLogger(JsonUtility.class);
+
+    public static JSONObject  getFileAsJsonObject(String filePath){
+        FileReader fileReader = null;
+        JSONObject jsonObject = null;
+        try {
+            fileReader = new FileReader(filePath);
+        }catch (FileNotFoundException f)
+        {
+            logger.error(f.getMessage());
+        }
+        JSONParser jsonParser = new JSONParser();
+        try {
+          jsonObject = (JSONObject) jsonParser.parse(fileReader);
+        }catch (ParseException | IOException i)
+        {
+            logger.error(i.getMessage());
+        }
+        return jsonObject;
+    }
+
+}
