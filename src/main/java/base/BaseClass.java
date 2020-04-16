fc873dfedcf1a5438e9dfb160936098c65a08424
love <lovsoni607@gmail.com>
2020-04-16 19:48:57 +0530

    Format base class

--- src/main/java/base/BaseClass.java
+++ src/main/java/base/BaseClass.java
@@ -6,6 +6,7 @@ package base;
 import org.apache.log4j.Logger;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
 import sessions.AndroidSession;
 import sessions.IOSSession;
 import sessions.SessionManager;
@@ -31,15 +32,12 @@ public class BaseClass {
 
     public void initializeSession() {
         String platformOs = EnvironmentFields.getPlatformOs();
-        logger.info("Platform OS On Which Test To Run - "+platformOs);
+        logger.info("Platform OS On Which Test To Run - " + platformOs);
         if (platformOs.equalsIgnoreCase("android")) {
             sessionManager = new AndroidSession();
-        }
-        else if(platformOs.equalsIgnoreCase("ios"))
-        {
+        } else if (platformOs.equalsIgnoreCase("ios")) {
             sessionManager = new IOSSession();
         }
     }
-
 
 }
