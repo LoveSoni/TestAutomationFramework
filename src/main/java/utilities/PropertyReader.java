0301d290a8b724cffc7d56ba6fb30e7ae9c8f6f9
love <lovesoni607@gmail.com>
2020-04-16 19:51:29 +0530

    rm unused import

--- src/main/java/utilities/PropertyReader.java
+++ src/main/java/utilities/PropertyReader.java
@@ -15,30 +15,30 @@ public class PropertyReader {
     private static Properties properties;
     private static Logger logger = Logger.getLogger(PropertyReader.class);
 
-    public static Properties readProperty(String filePath){
+    public static Properties readProperty(String filePath) {
         try {
             fileInputStream = new FileInputStream(filePath);
             properties = new Properties();
             properties.load(fileInputStream);
-        }catch (IOException f){
+        } catch (IOException f) {
             logger.error(f.getMessage());
         }
         return properties;
     }
 
-    public static Map<String,String> getAllKeysAndValues(String filePath){
+    public static Map<String, String> getAllKeysAndValues(String filePath) {
         Properties properties = readProperty(filePath);
-        Map<String,String> map = new HashMap<>();
+        Map<String, String> map = new HashMap<>();
         Set propertySet = properties.entrySet();
         Iterator iterator = propertySet.iterator();
-        while (iterator.hasNext()){
-            Map.Entry<String,String> entry = (Map.Entry) iterator.next();
-            map.put(entry.getKey(),entry.getValue());
+        while (iterator.hasNext()) {
+            Map.Entry<String, String> entry = (Map.Entry) iterator.next();
+            map.put(entry.getKey(), entry.getValue());
         }
         return map;
     }
 
-    public static Properties readLocatorProperties(String filePath){
+    public static Properties readLocatorProperties(String filePath) {
         return readProperty(Constants.LOCATORS_DIRECTORY_PATH + Constants.slash + filePath);
     }
 }
