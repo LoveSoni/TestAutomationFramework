d02b2b135e571214a6b68176aa114acef7ac8a0b
love <lovesoni607@gmail.com>
2020-04-16 19:50:42 +0530

    format java utility

--- src/main/java/utilities/JavaUtility.java
+++ src/main/java/utilities/JavaUtility.java
@@ -9,10 +9,10 @@ import org.apache.log4j.Logger;
 public class JavaUtility {
     private static Logger logger = Logger.getLogger(JavaUtility.class);
 
-    public static void sleep(int seconds){
-        try{
-            Thread.sleep(1000*seconds);
-        }catch (InterruptedException e){
+    public static void sleep(int seconds) {
+        try {
+            Thread.sleep(1000 * seconds);
+        } catch (InterruptedException e) {
             logger.error(e.getMessage());
         }
     }
