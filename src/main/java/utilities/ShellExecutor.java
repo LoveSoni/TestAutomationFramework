0d79e635e109d159e1ee124f8d1effc5ea7e2d9f
love <lovsoni607@gmail.com>
2020-04-18 18:21:44 +0530

    execute Command

--- src/main/java/utilities/ShellExecutor.java
+++ src/main/java/utilities/ShellExecutor.java
@@ -1,7 +1,31 @@
 package utilities;
 
-public class ShellExecutor {
-    public static void executeCommand(){
+import org.apache.log4j.Logger;
+import org.testng.annotations.Test;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class ShellExecutor {
+    private Logger logger = Logger.getLogger(ShellExecutor.class);
+
+    @Test
+    public static StringBuilder executeCommand(String command){
+        StringBuilder output = new StringBuilder();
+        try {
+           Process process =  Runtime.getRuntime().exec(command);
+           String line;
+           BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+           while((line = bufferedReader.readLine())!=null){
+                output.append(line+"\n");
+           }
+           System.out.print(output.toString());
+        }catch (IOException e){
+
+        }
+        return output;
     }
+
+
 }
