8a411d18f4821b34b358751ba5f327c64fbbc5c5
love <lovesoni607@gmail.com>
2020-04-18 18:44:03 +0530

    separate properties for shell commands

--- src/main/java/utilities/ShellExecutor.java
+++ src/main/java/utilities/ShellExecutor.java
@@ -1,6 +1,7 @@
 package utilities;
 
 import org.apache.log4j.Logger;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -8,24 +9,32 @@ import java.io.InputStreamReader;
 public class ShellExecutor {
     private Logger logger = Logger.getLogger(ShellExecutor.class);
 
-    public static StringBuilder executeCommand(String command){
+    public static StringBuilder executeCommand(String command) {
         StringBuilder output = new StringBuilder();
         try {
-           Process process =  Runtime.getRuntime().exec(command);
-           String line;
-           BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(process.getInputStream()));
-           while((line = bufferedReader.readLine())!=null){
-                output.append(line+"\n");
-           }
-           System.out.print(output.toString());
-        }catch (IOException e){
+            Process process = Runtime.getRuntime().exec(command);
+            String line;
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+            while ((line = bufferedReader.readLine()) != null) {
+                output.append(line + "\n");
+            }
+        } catch (IOException e) {
 
         }
         return output;
     }
 
-    public void getListOFDevices(){
+    public static void getListOfConnectedDevices() {
+        String platformOs = EnvironmentFields.getPlatformOs();
+        if (platformOs.equalsIgnoreCase("android")) {
+            System.out.print(executeCommand("adb devices"));
+        } else if (platformOs.equalsIgnoreCase("ios")) {
 
+        }
+    }
+
+    public static void main(String args[]) {
+        getListOfConnectedDevices();
     }
 
 
