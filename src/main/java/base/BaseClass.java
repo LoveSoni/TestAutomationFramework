4b1893a1ca39502f6146865602604610a5ca5091
love <lovesoni607@gmail.com>
2020-04-21 00:03:56 +0530

    get call in Rest Assured

--- src/main/java/base/BaseClass.java
+++ src/main/java/base/BaseClass.java
@@ -6,7 +6,6 @@ package base;
 import org.apache.log4j.Logger;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.Test;
 import sessions.AndroidSession;
 import sessions.IOSSession;
 import sessions.SessionManager;
--- src/main/java/utilities/RestAssuredClient.java
+++ src/main/java/utilities/RestAssuredClient.java
@@ -1,10 +1,34 @@
 package utilities;
 
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
+import org.apache.log4j.Logger;
+import org.testng.annotations.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * author Love
  */
 
 public class RestAssuredClient {
+    private Logger logger = Logger.getLogger(RestAssuredClient.class);
+
+    @Test
+    public void getRequest(){
+        RestAssured.baseURI = "https://gorest.co.in";
+        RequestSpecification requestSpecification = RestAssured.given();
+        Map<String,String> queryParams = new HashMap<>();
+        queryParams.put("first_name","first");
+        requestSpecification.queryParams(queryParams);
+        requestSpecification.contentType(ContentType.JSON);
+        Response response = requestSpecification.post("public-api/users");
+        logger.info("Status code is :"+response.statusCode());
+        logger.info("Response is: "+response.getBody().asString());
+    }
 
 }
 
