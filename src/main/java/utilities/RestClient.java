fb90b8316590b311bcf5483faf71727bac05de34
love <lovesoni607@gmail.com>
2020-04-20 22:30:09 +0530

    tst file

--- src/main/java/utilities/RestClient.java
+++ src/main/java/utilities/RestClient.java
@@ -1,4 +1,23 @@
 package utilities;
 
-public class RestClient {
+import java.util.Comparator;
+import java.util.TreeSet;
+
+public class RestClient implements Comparator {
+
+    public int compare(Object obj1,Object obj2){
+       String i = (String)obj1;
+       String i2 = (String)obj2;
+       return -i.compareTo(i2);
+    }
+  public static void main(String args[]){
+      TreeSet treeSet = new TreeSet(new RestClient());
+      treeSet.add("hii");
+      treeSet.add("asdb");
+      treeSet.add("demo");
+      treeSet.add("slash");
+      treeSet.add("peek");
+      System.out.print(treeSet);
+  }
 }
+
