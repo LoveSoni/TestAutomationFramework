aca258a5549e832126e9fb13f78a3c622bf7077a
love <lovesoni607@gmail.com>
2020-04-21 19:35:48 +0530

    Added template for httpclient

--- src/main/java/utilities/HttpClient.java
+++ src/main/java/utilities/HttpClient.java
@@ -4,10 +4,16 @@ package utilities;
  * author Love
  */
 
+import org.apache.http.HttpResponse;
+import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.utils.URIBuilder;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
 import org.apache.log4j.Logger;
 import org.testng.annotations.Test;
 
+import java.io.IOException;
 import java.net.URISyntaxException;
 import java.util.HashMap;
 import java.util.Map;
@@ -17,11 +23,30 @@ public class HttpClient {
 
     @Test
     public void getRequest(){
+        HttpResponse httpResponse = null;
+        String response = null;
         String url =  "https://gorest.co.in";
         Map<String,String> queryParams = new HashMap<>();
         queryParams.put("first_name","first");
-         url = buildUrlUsingParam(url,queryParams);
-         logger.info("URL - "+url);
+        url = buildUrlUsingParam(url,queryParams);
+        logger.info("URL - "+url);
+        CloseableHttpClient httpClient = HttpClients.createDefault();
+        HttpGet httpGet = new HttpGet(url);
+        httpGet.addHeader("content-type","application/json");
+        try {
+            httpResponse = httpClient.execute(httpGet);
+        }catch (IOException e)
+        {
+            logger.error(e.getMessage());
+        }
+        logger.info("Status code - "+httpResponse.getStatusLine().getStatusCode());
+        try {
+            response = EntityUtils.toString(httpResponse.getEntity());
+        }catch (IOException e)
+        {
+            logger.error(e.getMessage());
+        }
+        logger.info("Response - "+ response.toString());
     }
 
     public String buildUrlUsingParam(String url, Map<String,String> queryParams)
