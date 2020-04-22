bbf7faf62d6b52948012ac2892caf926534b8cf8
love <lovesoni607@gmail.com>
2020-04-22 16:10:06 +0530

    Api class

--- src/main/java/ApiSession/Api.java
+++ src/main/java/ApiSession/Api.java
@@ -2,92 +2,71 @@ package ApiSession;
 
 
 import org.json.simple.JSONObject;
-import java.util.HashMap;
 import java.util.Map;
 
 public class Api {
 
-    public Api(String httpMethod, String url, String path, Map<String, String> headers) {
-        this.httpMethod = httpMethod;
-        this.url = url;
-        this.path = path;
-        this.headers = headers;
-    }
+    public Api(){
 
-    public Api(String httpMethod, String url, String path,JSONObject requestJson, Map<String, String> headers) {
-        this.httpMethod = httpMethod;
-        this.url = url;
-        this.path = path;
-        this.headers = headers;
-        this.requestJson = requestJson;
-    }
-
-    public Api(String httpMethod,String url,String path, Map<String,String> headers,Map<String,String> queryParams) {
-        this.httpMethod = httpMethod;
-        this.url = url;
-        this.path = path;
-        this.headers = headers;
-        this.queryParams = queryParams;
-    }
-
-    public String getHttpMethod() {
-        return httpMethod;
-    }
-
-    public void setHttpMethod(String httpMethod) {
-        this.httpMethod = httpMethod;
-    }
-
-    public String getUrl() {
-        return url;
-    }
-
-    public void setUrl(String url) {
-        this.url = url;
-    }
-
-    public Map<String, String> getQueryParams() {
-        return queryParams;
-    }
-
-    public void setQueryParams(Map<String, String> queryParams) {
-        this.queryParams = queryParams;
-    }
-
-    public String getPath() {
-        return path;
-    }
-
-    public void setPath(String path) {
-        this.path = path;
-    }
-
-    public Map<String, String> getHeaders() {
-        return headers;
-    }
-
-    public void setHeaders(Map<String, String> headers) {
-        this.headers = headers;
-    }
-
-    public JSONObject getRequestJson() {
-        return requestJson;
-    }
-
-    public void setRequestJson(JSONObject requestJson) {
-        this.requestJson = requestJson;
     }
 
     private String httpMethod;
 
     private String url;
 
-    private Map<String, String> queryParams;
-
     private String path;
+
+    private Map<String, String> queryParams;
 
     private Map<String, String> headers;
 
     private JSONObject requestJson;
+
+    public void setHttpMethod(String httpMethod) {
+        this.httpMethod = httpMethod;
+    }
+
+    public String getHttpMethod() {
+        return httpMethod;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setPath(String path) {
+        this.path = path;
+    }
+
+    public String getPath() {
+        return path;
+    }
+
+    public void setQueryParams(Map<String, String> queryParams) {
+        this.queryParams = queryParams;
+    }
+
+    public Map<String, String> getQueryParams() {
+        return queryParams;
+    }
+
+    public void setHeaders(Map<String, String> headers) {
+        this.headers = headers;
+    }
+
+    public Map<String, String> getHeaders() { return headers; }
+
+    public void setRequestJson(JSONObject requestJson) {
+        this.requestJson = requestJson;
+    }
+
+    public JSONObject getRequestJson() {
+        return requestJson;
+    }
+
 
 }
