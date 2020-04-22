0ddffeb642a4f290d868628fa75433624277940c
love <lovesoni607@gmail.com>
2020-04-22 23:32:00 +0530

    get httpmethod in httpclient

--- src/main/java/ApiSession/HttpSession.java
+++ src/main/java/ApiSession/HttpSession.java
@@ -3,17 +3,40 @@ package ApiSession;
 /**
  * author Love
  */
-
+import org.apache.http.client.methods.*;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
 import org.apache.log4j.Logger;
 
 public class HttpSession {
     private Logger logger = Logger.getLogger(HttpSession.class);
 
     public void sendRequest(Api api){
+        CloseableHttpClient httpClient = HttpClients.createDefault();
 
     }
 
-    public String getHttpMethod(Api api){
+    public HttpRequestBase getHttpMethod(Api api){
+        String httpMethod = api.getHttpMethod();
+        HttpRequestBase httpRequestBase = null;
+        switch (httpMethod){
+            case "GET":
+                httpRequestBase = new HttpGet();
+                break;
+            case "POST":
+                httpRequestBase = new HttpPost();
+                break;
+            case "PUT":
+                httpRequestBase = new HttpPut();
+                break;
+            case "PATCH":
+                httpRequestBase = new HttpPatch();
+                break;
+        }
+        return httpRequestBase;
+    }
+
+    public String setHttpHeaders(Api api){
         return "";
     }
 
