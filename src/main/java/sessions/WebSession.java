e4ce4d14d469e742c71ce472308d57555813bdb2
love <lovesoni607@gmail.com>
2020-04-27 22:47:22 +0530

    set logger in websession class

--- src/main/java/sessions/WebSession.java
+++ src/main/java/sessions/WebSession.java
@@ -4,12 +4,14 @@ package sessions;
  * author Love
  */
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.apache.log4j.Logger;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import utilities.EnvironmentFields;
 
 public class WebSession {
     private WebDriver driver;
+    private Logger logger = Logger.getLogger(WebSession.class);
     private WebDriverManager webDriverManager;
     private String browserName = EnvironmentFields.getBrowserName();
 
