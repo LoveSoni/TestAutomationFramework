e493f16eeb2132914f48d0f57b4ddc50799efcff
love <lovesoni607@gmail.com>
2020-04-27 23:27:59 +0530

    added weblibrary

---
+++ src/main/java/library/WebLibrary.java
@@ -0,0 +1,12 @@
+package library;
+
+import org.openqa.selenium.WebDriver;
+import sessions.WebSession;
+
+public class WebLibrary {
+    private WebDriver driver;
+
+    public WebLibrary(WebSession webSession){
+        driver = webSession.getDriver();
+    }
+}
