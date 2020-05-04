f016365ee81ed49babc39c8583f5b32e848f7bd7
love <lovesoni607@gmail.com>
2020-05-04 23:16:46 +0530

    get and flush extent report

--- src/main/java/utilities/ExtentUtility.java
+++ src/main/java/utilities/ExtentUtility.java
@@ -1,13 +1,26 @@
 package utilities;
 
+import com.aventstack.extentreports.ExtentReports;
 import com.aventstack.extentreports.reporter.ExtentHtmlReporter;
 import constants.Constants;
+import constants.Defaults;
 
 public class ExtentUtility {
-
-    public void createInstance(){
+    private ExtentReports extentReports;
+    public void initializeReports(){
         ExtentHtmlReporter extentHtmlReporter = new ExtentHtmlReporter(Constants.EXTENT_REPORT_PATH);
+        extentHtmlReporter.config().setReportName(Defaults.EXTENT_REPORT_NAME);
+        extentHtmlReporter.config().setDocumentTitle(Defaults.EXTENT_DOCUEMTN_TITILE);
+        extentReports = new ExtentReports();
+        extentReports.attachReporter(extentHtmlReporter);
+    }
 
+    public ExtentReports getExtent(){
+        return this.extentReports;
+    }
+
+    public void flushExtent(){
+        extentReports.flush();
     }
 
 }
