ae7d9015e2b8c4992e7881fa453942061caa4614
love <lovesoni607@gmail.com>
2020-05-04 23:16:03 +0530

    set report name and document title in defaults

--- src/main/java/constants/Defaults.java
+++ src/main/java/constants/Defaults.java
@@ -9,4 +9,8 @@ public class Defaults {
     private static String slash = File.separator;
 
     public static String DEFAULT_ENVIRONMENT_PATH = System.getProperty("user.dir") + slash + "src" + slash + "main" + slash + "resources" + slash + "environments" + slash + "defaultEnvironment.properties";
+
+    public static String EXTENT_REPORT_NAME = "APP_AUTOMATION";
+
+    public static String EXTENT_DOCUEMTN_TITILE = "APP";
 }
