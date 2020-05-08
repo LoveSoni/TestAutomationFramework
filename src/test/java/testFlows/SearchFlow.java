0b882d9943523687b51b98fa1765e0bcd464f47e
love <lovesoni607@gmail.com>
2020-05-08 22:59:02 +0530

    format search flow

--- src/test/java/testFlows/SearchFlow.java
+++ src/test/java/testFlows/SearchFlow.java
@@ -13,57 +13,58 @@ public class SearchFlow {
     private AppPageSteps appPageSteps;
     private Logger logger = Logger.getLogger(SearchFlow.class);
 
-    public SearchFlow(SessionManager sessionManager){
+    public SearchFlow(SessionManager sessionManager) {
         searchSteps = new SearchSteps(sessionManager);
         appPageSteps = new AppPageSteps(sessionManager);
         mainPageSteps = new MainPageSteps(sessionManager);
     }
 
-    public void selectInvokeSearch(){
+    public void selectInvokeSearch() {
         moveToSearchoptions();
         logger.info("Select Invoke Search ");
         searchSteps.selectInvokeSearch();
     }
 
-    public void selectQuerySearchResult(){
+    public void selectQuerySearchResult() {
         moveToSearchoptions();
         logger.info("Search Query Search Result");
         searchSteps.selectQuerySearhResults();
     }
 
-    public void moveToSearchoptions(){
+    public void moveToSearchoptions() {
         logger.info("Select App option from Main Page");
         mainPageSteps.selectAppOption();
         logger.info("Select Search Option ");
         appPageSteps.selectSearch();
     }
 
-    public void enterPrefilQueryAndAppData(String prefilText,String appData){
+    public void enterPrefilQueryAndAppData(String prefilText, String appData) {
         logger.info("Enter prefil query");
         searchSteps.enterPrefilQuery(prefilText);
         logger.info("Enter App data");
         searchSteps.enterAppData(appData);
     }
 
-    public void enterOnlyPrefilQuery(String prefixText){
+    public void enterOnlyPrefilQuery(String prefixText) {
         logger.info("Enter Prefil Query");
         searchSteps.enterPrefilQuery(prefixText);
     }
 
-    public String clickOnRequestedAndGetSearchText(){
+    public String clickOnRequestedAndGetSearchText() {
         searchSteps.clickOnSearchRequested();
         return searchSteps.getSearchText();
     }
-    public boolean isSearchRequestedDisplay(){
+
+    public boolean isSearchRequestedDisplay() {
         return searchSteps.isOnSearchDisplayed();
     }
 
-    public void onSearchAndEnterText(String searchText){
+    public void onSearchAndEnterText(String searchText) {
         searchSteps.clickOnSearchRequested();
         searchSteps.enterTextInSearchField(searchText);
     }
 
-    public String fetchQueryTitle(){
+    public String fetchQueryTitle() {
         return searchSteps.getQuerySearchTitle();
     }
 }
