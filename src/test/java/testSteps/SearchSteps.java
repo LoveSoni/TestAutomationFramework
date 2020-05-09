ac7515346550810783af3ec6ffbf6ad44ea75779
love <lovesoni607@gmail.com>
2020-05-09 22:14:15 +0530

    format search steps

--- src/test/java/testSteps/SearchSteps.java
+++ src/test/java/testSteps/SearchSteps.java
@@ -8,6 +8,7 @@ import constants.Constants;
 import library.AppiumLibrary;
 import sessions.SessionManager;
 import utilities.PropertyReader;
+
 import java.util.Properties;
 
 public class SearchSteps {
@@ -30,26 +31,27 @@ public class SearchSteps {
         appiumLibrary.click(searchScreen.getProperty("onSearchRequested"));
     }
 
-    public boolean isOnSearchDisplayed(){
+    public boolean isOnSearchDisplayed() {
         return appiumLibrary.isElementPresent(searchScreen.getProperty("onSearchRequested"), Constants.ELEMENT_EXPLICIT_WAIT);
     }
+
     public void enterPrefilQuery(String text) {
         appiumLibrary.enterText(searchScreen.getProperty("enterPrefilQuery"), text);
     }
 
-    public void enterAppData(String text){
-        appiumLibrary.enterText(searchScreen.getProperty("appData"),text);
+    public void enterAppData(String text) {
+        appiumLibrary.enterText(searchScreen.getProperty("appData"), text);
     }
 
-    public void enterTextInSearchField(String text){
-        appiumLibrary.enterText(searchScreen.getProperty("searchedText"),text);
+    public void enterTextInSearchField(String text) {
+        appiumLibrary.enterText(searchScreen.getProperty("searchedText"), text);
     }
 
-    public String getSearchText(){
+    public String getSearchText() {
         return appiumLibrary.getTextAttribute(searchScreen.getProperty("searchedText"));
     }
 
-    public String getQuerySearchTitle(){
+    public String getQuerySearchTitle() {
         return appiumLibrary.getTextAttribute(searchScreen.getProperty("queryTitle"));
     }
 
