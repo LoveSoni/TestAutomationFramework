f5ea2d0e09c8ac83415fae595390980870fa96f1
love <love lovesoni607@gmail.com>
2020-05-13 23:14:22 +0530

    initiate class in logutility

--- src/main/java/utilities/LogUtility.java
+++ src/main/java/utilities/LogUtility.java
@@ -1,4 +1,13 @@
 package utilities;
 
+import org.apache.log4j.Logger;
+
 public class LogUtility {
+    private Logger logger;
+
+    public LogUtility(Class clasName){
+        logger = Logger.getLogger(clasName);
+    }
+
+
 }
