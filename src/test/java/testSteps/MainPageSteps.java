abc4753432a1a27e1d9f8398654c28061f8f08ae
love <lovesoni607@gmail.com>
2020-05-16 23:42:55 +0530

    format main pages

--- src/test/java/testSteps/MainPageSteps.java
+++ src/test/java/testSteps/MainPageSteps.java
@@ -10,51 +10,51 @@ public class MainPageSteps {
     private AppiumLibrary appiumLibrary;
     private Properties mainPage = PropertyReader.readLocatorProperties("mainScreen.properties");
 
-    public MainPageSteps(SessionManager sessionManager){
+    public MainPageSteps(SessionManager sessionManager) {
         appiumLibrary = new AppiumLibrary(sessionManager);
     }
 
-    public void selectAccessibilityOption(){
+    public void selectAccessibilityOption() {
         appiumLibrary.click(mainPage.getProperty("accessibility"));
     }
 
-    public void selectAnimationOption(){
+    public void selectAnimationOption() {
         appiumLibrary.click(mainPage.getProperty("animation"));
     }
 
-    public void selectAppOption(){
+    public void selectAppOption() {
         appiumLibrary.click(mainPage.getProperty("appOption"));
     }
 
-    public void selectContentOption(){
+    public void selectContentOption() {
         appiumLibrary.click(mainPage.getProperty("content"));
     }
 
-    public void selectGraphicsOption(){
+    public void selectGraphicsOption() {
         appiumLibrary.click(mainPage.getProperty("graphics"));
     }
 
-    public void selectMediaOption(){
+    public void selectMediaOption() {
         appiumLibrary.click(mainPage.getProperty("media"));
     }
 
-    public void selectNFCOption(){
+    public void selectNFCOption() {
         appiumLibrary.click(mainPage.getProperty("nfc"));
     }
 
-    public void selectOSOption(){
+    public void selectOSOption() {
         appiumLibrary.click(mainPage.getProperty("os"));
     }
 
-    public void selectPreferenceOption(){
+    public void selectPreferenceOption() {
         appiumLibrary.click(mainPage.getProperty("preference"));
     }
 
-    public void selectTextOption(){
+    public void selectTextOption() {
         appiumLibrary.click(mainPage.getProperty("text"));
     }
 
-    public void selectViewOption(){
+    public void selectViewOption() {
         appiumLibrary.click(mainPage.getProperty("views"));
     }
 }
