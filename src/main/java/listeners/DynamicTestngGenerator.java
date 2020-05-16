37969597239dc570b73e8d72b9a216aabc3bbf70
love <lovesoni607@gmail.com>
2020-05-16 23:10:34 +0530

    added dynamic testng template

--- src/main/java/listeners/DynamicTestngGenerator.java
+++ src/main/java/listeners/DynamicTestngGenerator.java
@@ -1,4 +1,10 @@
 package listeners;
 
+import org.testng.TestNG;
+import org.testng.xml.XmlSuite;
+
 public class DynamicTestngGenerator {
+    public static void main(String args[]){
+        XmlSuite xmlSuite = new XmlSuite();
+    }
 }
