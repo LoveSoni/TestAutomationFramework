19e0916c2b5833f0de225c0443f1cd21fc15eb08
love <lovesoni607@gmail.com>
2020-05-19 23:21:37 +0530

    remove commented code

--- src/test/java/testClasses/DynamicTestngGenerator.java
+++ src/test/java/testClasses/DynamicTestngGenerator.java
@@ -8,9 +8,13 @@ import org.testng.xml.XmlClass;
 import org.testng.xml.XmlSuite;
 import org.testng.xml.XmlTest;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class DynamicTestngGenerator implements ISuiteListener {
     @Override
@@ -23,47 +27,43 @@ public class DynamicTestngGenerator implements ISuiteListener {
         }
     }
 
-    //    @Override
-//    public void alter(List<XmlSuite> suites){
-//        System.out.println(suites.toString());
-//    }
     public static void main(String args[]){
-//        XmlSuite xmlSuite = new XmlSuite();
-//        xmlSuite.setName("dynamic suite");
-//        xmlSuite.setParallel(XmlSuite.ParallelMode.TESTS);
-//        xmlSuite.setThreadCount(2);
-//
-//        XmlTest xmlTest = new XmlTest(xmlSuite);
-//        xmlTest.setName("parallel test");
-//        Map<String,String> parameters = new HashMap<>();
-//        parameters.put("udid","emulator-5554");
-//        xmlTest.setParameters(parameters);
-//
-//        List<XmlClass> xmlClasses = new ArrayList<>();
-//        XmlClass xmlClass = new XmlClass(LoginTest.class);
-//        xmlClasses.add(xmlClass);
-//
-//        xmlTest.setXmlClasses(xmlClasses);
-//
-//
-//        List<XmlSuite> xmlSuites = new ArrayList<>();
-//        xmlSuites.add(xmlSuite);
-//
-//
-//        TestNG testNG = new TestNG();
-//        testNG.setXmlSuites(xmlSuites);
-//        testNG.run();
+        XmlSuite xmlSuite = new XmlSuite();
+        xmlSuite.setName("dynamic suite");
+        xmlSuite.setParallel(XmlSuite.ParallelMode.TESTS);
+        xmlSuite.setThreadCount(2);
 
-//        FileWriter writer;
-//        try {
-//            writer = new FileWriter(new File("myVirtual.xml"));
-//            writer.write(xmlSuite.toXml());
-//            writer.flush();
-//            writer.close();
-//            System.out.println(new File("myVirtual.xml").getAbsolutePath());
-//        } catch (IOException e) {
-//
-//            e.printStackTrace();
-//        }
+        XmlTest xmlTest = new XmlTest(xmlSuite);
+        xmlTest.setName("parallel test");
+        Map<String,String> parameters = new HashMap<>();
+        parameters.put("udid","emulator-5554");
+        xmlTest.setParameters(parameters);
+
+        List<XmlClass> xmlClasses = new ArrayList<>();
+        XmlClass xmlClass = new XmlClass(SearchTest.class);
+        xmlClasses.add(xmlClass);
+
+        xmlTest.setXmlClasses(xmlClasses);
+
+
+        List<XmlSuite> xmlSuites = new ArrayList<>();
+        xmlSuites.add(xmlSuite);
+
+
+        TestNG testNG = new TestNG();
+        testNG.setXmlSuites(xmlSuites);
+        testNG.run();
+
+        FileWriter writer;
+        try {
+            writer = new FileWriter(new File("myVirtual.xml"));
+            writer.write(xmlSuite.toXml());
+            writer.flush();
+            writer.close();
+            System.out.println(new File("myVirtual.xml").getAbsolutePath());
+        } catch (IOException e) {
+
+            e.printStackTrace();
+        }
     }
 }
