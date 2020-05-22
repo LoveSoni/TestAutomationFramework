50cac184b21b2affd3ae7325149867d1b728f58f
love <lovesoni607@gmail.com>
2020-05-22 16:21:41 +0530

    prepare xml suite

--- src/main/java/listeners/TestngGenerator.java
+++ src/main/java/listeners/TestngGenerator.java
@@ -1,13 +1,26 @@
 package listeners;
 
 import org.testng.annotations.Test;
+import org.testng.xml.XmlSuite;
 import utilities.ShellExecutor;
 
+import java.util.List;
+
 public class TestngGenerator {
+    private List<String> udidList =ShellExecutor.getListOfConnectedDevices();
+    private int numberOfDeviceConnected = udidList.size();
 
     @Test
     public void generateTestng(){
-        System.out.println(ShellExecutor.getListOfConnectedDevices());
+
+    }
+
+    public XmlSuite prepareXmlSuite(){
+        XmlSuite xmlSuite = new XmlSuite();
+        xmlSuite.setName("Dynamic Suite");
+        xmlSuite.setParallel(XmlSuite.ParallelMode.TESTS);
+        xmlSuite.setThreadCount(numberOfDeviceConnected);
+        return xmlSuite;
     }
 
 }
