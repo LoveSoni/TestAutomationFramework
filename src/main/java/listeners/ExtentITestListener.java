8eca9c22c6ce2361a026343743677a22bf79ba45
love <lovesoni607@gmail.com>
2020-05-27 13:14:46 +0530

    log test status in extent report

--- src/main/java/listeners/ExtentITestListener.java
+++ src/main/java/listeners/ExtentITestListener.java
@@ -1,20 +1,29 @@
 package listeners;
 
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
 import org.testng.ITestContext;
 import org.testng.ITestListener;
 import org.testng.ITestResult;
+import utilities.ExtentUtility;
 
 public class ExtentITestListener implements ITestListener {
+    private ExtentReports extentReports = ExtentUtility.initializeReports();
+    private ExtentTest extentTest;
+
     @Override
     public void onTestStart(ITestResult result) {
+        extentTest = extentReports.createTest(result.getMethod().getMethodName());
     }
 
     @Override
     public void onTestSuccess(ITestResult result) {
+        extentTest.pass("Test is passed");
     }
 
     @Override
     public void onTestFailure(ITestResult result) {
+        extentTest.pass("Test is failed");
     }
 
     @Override
