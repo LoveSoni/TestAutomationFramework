f3b16d2573ad91344f76824d0fa1db9dc5c4b2dd
love <lovesoni607@gmail.com>
2020-05-27 13:07:26 +0530

    use extent utility

--- src/main/java/utilities/ExtentUtility.java
+++ src/main/java/utilities/ExtentUtility.java
@@ -6,9 +6,9 @@ import constants.Constants;
 import constants.Defaults;
 
 public class ExtentUtility {
-    private ExtentReports extentReports;
+    private static ExtentReports extentReports;
 
-    public void initializeReports() {
+    public static void initializeReports() {
         ExtentHtmlReporter extentHtmlReporter = new ExtentHtmlReporter(Constants.EXTENT_REPORT_PATH);
         extentHtmlReporter.config().setReportName(Defaults.EXTENT_REPORT_NAME);
         extentHtmlReporter.config().setDocumentTitle(Defaults.EXTENT_DOCUEMTN_TITILE);
