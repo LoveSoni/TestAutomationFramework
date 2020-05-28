d7184e4033c12e3f5fa9e1cbb197ca610cec40f4
love <lovesoni607@gmail.com>
2020-05-28 13:40:40 +0530

    use testng.xml to run test

---
+++ src/test/java/testClasses/LoginTest.java
@@ -0,0 +1,15 @@
+package testClasses;
+
+import org.testng.annotations.Test;
+
+public class Demo1 {
+
+    @Test
+    public void demo11(){
+    }
+
+    @Test
+    public void demo12(){
+
+    }
+}
